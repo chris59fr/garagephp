70aec0d675cfce4b0e60aad0be7146b8bf5804ab
chris59fr <nordweb.freelance@gmail.com>
2025-07-08 15:02:34 +0200

    modification de la class config

--- config/Config.php
+++ config/Config.php
@@ -1,9 +1,12 @@
 <?php
 
+namespace App\Config;
+use Dotenv\Dotenv;
 
 class Config{
 
-
+    /*Class Config manuelle, cette classe sert a charger le fichier .env,
+         à le lire et séparer et nettoyer les données 
     private static array $config = [];
     private static bool $loaded = false;
 
@@ -52,6 +55,29 @@ class Config{
 
     public static function isDebug():bool{
         return self::get('APP_DEBUG', 'false') === 'true';
+    }*/
+
+    /** 
+     * @param string $path le chemin vers le dossier contenant le fichier .env
+    */
+
+    public static function load($path = __DIR__ . '../'):void{
+
+        //on verifie si le fichier .env existe avant de tenter de le charger
+        if(file_exists($path . '.env')){
+            $dotenv = Dotenv::createImmutable($path);
+            $dotenv->load();
+        }
     }
+
+    /**
+     * @param string $key le nom de la variable 
+     * @param mixed $default une valeur par défaut à retourner si la variable n'existe pas
+     * @return mixed la valeur de la variable ou la valeur par defaut
+     */
+
+     public static function get(string $key, $default = null){
+        return $_ENV[$key] ?? $default;
+     }
 
 }
